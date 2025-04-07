6a944f17df5e692e197a008d0c592d538e27d2c6
Alcides Paz <alcidesnp@hotmail.com>
2025-04-07 20:33:00 -0300

    update: Atualização dos script para inserção dos dados na tabela criada

--- Banco de Dados/Projeto_DML.sql
+++ Banco de Dados/Projeto_DML.sql
@@ -8,3 +8,32 @@ VALUES
 ('Mouse', 'Mouse Logitech', 99.90, 50, 'Informatica', ''),
 ('Teclado', 'Teclado Dell', 209.50, 100, 'Informatica', '')
 
+INSERT INTO Cliente (NomeCompleto, Email, Telefone, Endereco, DataCadastro)
+VALUES
+('Vinicio Santos', 'vinicio@senai.br', '(11) 999994444', 'Rua Niteroi, 180 - São Paulo/SP', '05/04/2022'),
+('Saulo Santos', 'saulo@senai.br', '(11) 22223344', 'Rua Niteroi, 250 - São Paulo/SP', '20/11/2022')
+
+INSERT INTO Pedido (IdCliente, DataPedido, Status, ValorTotal)
+VALUES
+(3, '06/05/2023', 'Processando', 3200.99),
+(4, '10/05/2023', 'Concluído', 450.90)
+
+
+INSERT INTO Pagamento (IdPedido, FormaPagamento, Status, Data)
+VALUES
+(3, 'Cartão de Crédito', 'Aprovado', '08/05/2023 12:30:00'),
+(4, 'Boleto', 'Aprovado', '18/05/2023 16:30:00')
+
+INSERT INTO ItensPedido (IdPedido, IdProduto, Quantidade)
+VALUES
+(3, 1, 2),
+(4, 2, 1),
+(3, 1, 1)
+
+
+DELETE FROM Cliente WHERE NomeCompleto = 'Vinicio Santos';
+
+-- DQL - Visualizar os dados
+-- Select
+
+SELECT * FROM Cliente;
