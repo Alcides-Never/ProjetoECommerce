a9f77489812ae7b4e16a44f55b35c6599ccbfeba
Alcides Paz <alcidesnp@hotmail.com>
2025-04-08 21:57:14 -0300

    update: Inicio da criação da API do projeto Ecommerce. Criado a models e a context

---
+++ ECommerceAPI/ECommerceAPI/Models/Produto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ECommerceAPI.Models;
+
+public partial class Produto
+{
+    public int IdProduto { get; set; }
+
+    public string? NomeProduto { get; set; }
+
+    public string? Descricao { get; set; }
+
+    public decimal? Preco { get; set; }
+
+    public int? EstoqueDisponivel { get; set; }
+
+    public string? Categoria { get; set; }
+
+    public string? Imagem { get; set; }
+
+    public virtual ICollection<ItensPedido> ItensPedidos { get; set; } = new List<ItensPedido>();
+}
